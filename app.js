33752d0abf4974e6364a3e33b2b8150c727d13a6
RilaKorila <murakami.ayana3@gmail.com>
2023-04-08 17:17:06 +0900

    show index.html & index.css

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(`${__dirname}/public`));
 // ROUTING
 app.get("/", (req, res) => {
   greeting = "<h1>Hello From Node on Fly!</h1>";
-  res.send(greeting);
+  res.sendFile("./public/index.html")
 });
 
 module.exports = app;

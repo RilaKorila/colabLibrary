077e5238d29f90ca6eba5b866830c2d49afcbe0d
RilaKorila <murakami.ayana3@gmail.com>
2023-04-08 21:09:49 +0900

    set eslint, prettier

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 // 1) MIDDLEWARES: stands between req and res
 // if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+app.use(morgan('dev'));
 // }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // ROUTING
-app.get("/", (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  res.sendFile("./public/index.html")
+app.get('/', (req, res) => {
+    greeting = '<h1>Hello From Node on Fly!</h1>';
+    res.sendFile('./public/index.html');
 });
 
 module.exports = app;

66f202725d81aff6a164a98fd734e98968db9235
RilaKorila <murakami.ayana3@gmail.com>
2023-04-08 17:18:24 +0900

    install morgan

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const morgan = require("morgan");
 const app = express();
 
 // 1) MIDDLEWARES: stands between req and res
-if (process.env.NODE_ENV === "development") {
+// if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-}
+// }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
